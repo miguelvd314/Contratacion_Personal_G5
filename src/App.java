20dce04d472a7b0d9d48abaf694a78297f8b43d2
EduardoEsc <60011241+EduardoEsc@users.noreply.github.com>
2020-08-30 21:22:47 -0500

    Revert "cambios"
    This reverts commit 3825dd771b8c1442801c93ce0f85122c0ee84602.

--- src/App.java
+++ src/App.java
@@ -19,7 +19,7 @@ public class App {
         
         ControladorIngreso  controlador = new ControladorIngreso(usuario, fIngreso, fRegistro, frrhh, vvacante);
         controlador.iniciar();
-         System. out. println(" Hola");       
+                
     }
     
    
